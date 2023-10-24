1a059dd2eebdf38f5a578b9aea60e168105e10e3
VDani3 <villacarmona.dani@gmail.com>
2023-10-24 16:08:22 +0200

    Cambiar Imagen acabado

--- app/src/main/java/com/dani/pr14_threads_de_comunicacio/MainActivity.java
+++ app/src/main/java/com/dani/pr14_threads_de_comunicacio/MainActivity.java
@@ -2,12 +2,16 @@ package com.dani.pr14_threads_de_comunicacio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -17,6 +21,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -31,26 +36,41 @@ public class MainActivity extends AppCompatActivity {
         String error = "";
         Button searchButton = (Button) findViewById(R.id.search);
         TextView mostrar = (TextView) findViewById(R.id.text);
+        ImageView  randomFoto = (ImageView) findViewById(R.id.foto);
+        Random r = new Random();
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
                 Log.i("miapp", "hooooooooooooooooooooooooo");
                 ExecutorService executor = Executors.newSingleThreadExecutor();
 
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
+                        int w = r.nextInt(400)+100;
+                        int h = r.nextInt(400)+100;
                         String ip = getDataFromUrl("https://animechan.xyz/api/random", error);
                         Log.i("MiApp",ip);
+                        String f = "https://placekitten.com/"+Integer.toString(w)+"/"+Integer.toString(h);
+                        Bitmap bm = null;
+
+                        try {
+                            InputStream in = new java.net.URL(f).openStream();
+                            bm = BitmapFactory.decodeStream(in);
+                        } catch (Exception e) {
+                            Log.e("Error", e.getMessage());
+                            e.printStackTrace();
+                        }
+
                         // Tasques en background (xarxa)
 
                         Handler handler = new Handler(Looper.getMainLooper());
+                        Bitmap finalBm = bm;
                         handler.post(new Runnable() {
                             @Override
                             public void run() {
+                                randomFoto.setImageIcon(Icon.createWithBitmap(finalBm));
                                 // Tasques a la interfície gràfica (GUI)
                                 mostrar.setText(ip);
 
